e5bca97277c910b4fa88ef3f1f588368651a6bb1
Maia-Nguyen <maialyn@csu.fullerton.edu>
2021-05-22 00:38:59 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Container from 'components/Container';
+
+const Header = () => {
+  return (
+    <header>
+      <Container type="content">
+        <p>Covid Map Central</p>
+        <ul>
+          <li>
+            <Link to="/page-2/">More Data</Link>
+          </li>
+          <li>
+            <Link to="https://www.vaccines.gov/search/" target="_blank">Vaccines</Link>
+          </li>
+        </ul>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
